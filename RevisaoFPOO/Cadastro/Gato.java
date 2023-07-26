642e3ca38e1bf5439b3f18dd054003c5eb42c330
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-07-25 21:57:07 -0300

    25/07/23

---
+++ RevisaoFPOO/Cadastro/Gato.java
@@ -0,0 +1,16 @@
+package RevisaoFPOO.Cadastro;
+
+public class Gato extends  Animal{
+  public Gato() {
+    super();
+  }
+    String raca;
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+}
---
+++ RevisaoFPOO/Cadastro/Outros.java
@@ -0,0 +1,17 @@
+package RevisaoFPOO.Cadastro;
+
+public class Outros extends Animal{
+public Outros() {
+    super();
+}
+    String raca;
+
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+}
