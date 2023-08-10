ac7eb74d8d55d67ef514752f50c073e528dd0494
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-08-09 21:57:11 -0300

    09/08

--- Custoviagem/Custoviagem.java
+++ Custoviagem/Custoviagem.java
@@ -2,17 +2,32 @@
 /**
  * CustoViagem
  */
-public class CustoViagem {
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+
+public class Custoviagem {
 
     public static void main(String[] args) {
-        int distanciaPercorrida = 148;// em km
-        double consumoTransporte = 10; // em km/l
-        double precoCombustivel = 4.99; // em R$
-        double quatidadeLitrosCombustivel = distanciaPercorrida/consumoTransporte;
-        double custoTotal = quatidadeLitrosCombustivel*precoCombustivel;
-        System.out.println("o Custo total da Viagem foi de:");
-        System.out.println("R$"+custoTotal);
+        // Valores da viagem
+        int distanciaPercorrida = Integer.parseInt(JOptionPane.showInputDialog("Digite a distância percorrida (em km):"));
+        double consumoTransporte = Double.parseDouble(JOptionPane.showInputDialog("Digite o consumo do transporte (em km/l):"));
+        double precoCombustivelPorLitro = Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do combustível (em R$/litro):"));
 
+        // Cálculo do custo total da viagem
+        double quantidadeLitrosCombustivel = distanciaPercorrida / consumoTransporte;
+        double custoTotal = quantidadeLitrosCombustivel * precoCombustivelPorLitro;
 
+        // Formatação do valor do custo total da viagem
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        String custoFormatado = "R$" + df.format(custoTotal);
+
+        // Mensagem com o resultado
+        String mensagem = "Distância percorrida: " + distanciaPercorrida + " km\n"
+                + "Consumo do transporte: " + consumoTransporte + " km/l\n"
+                + "Preço do combustível: R$" + precoCombustivelPorLitro + " por litro\n"
+                + "Quantidade de litros de combustível: " + df.format(quantidadeLitrosCombustivel) + " litros\n"
+                + "Custo total da viagem: " + custoFormatado;
+
+        JOptionPane.showMessageDialog(null, mensagem, "Resultado da Viagem", JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- Exception/ExemploTryCatch.java
+++ Exception/ExemploTryCatch.java
@@ -1,63 +1,63 @@
 package Exception;
 
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
 public class ExemploTryCatch {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Calculadora: Escolhas as operações");
- boolean rodando = true;
+        boolean rodando = true;
+        while (rodando)
+            try {
+                int acao1 = Integer.parseInt(JOptionPane.showInputDialog("1-Soma\n 2-Sub\n 3-Div\n 4-Mult\n 5-Sair "));
+                if (acao1 == 1) {// Soma
 
- while(rodando)
-        try {
-            int acao1 = Integer.parseInt(JOptionPane.showInputDialog("1-Soma\n 2-Sub\n 3-Div\n 4-Mult\n 5-Sair "));
-            if(acao1==1){//Soma
+                    int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
+                    int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
 
-           int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
-            int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
+                    Integer.parseInt(JOptionPane.showInputDialog(null, "O resultado "
+                            + (numero1 + numero2)));
+                }
 
-          Integer.parseInt(JOptionPane.showInputDialog(null,"O resultado "
-                                                                       +(numero1+numero2)));}
- 
-           else if(acao1==2){//Soma
+                else if (acao1 == 2) {// Soma
 
-           int numero3 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
-            int numero4 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
+                    int numero3 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
+                    int numero4 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
 
-          Integer.parseInt(JOptionPane.showInputDialog(null,"O resultado "
-                                                                       +(numero3-numero4)));}
-         else   if(acao1==3){//Soma                                                            
- int numero5= Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
-            int numero6 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
+                    Integer.parseInt(JOptionPane.showInputDialog(null, "O resultado "
+                            + (numero3 - numero4)));
+                } else if (acao1 == 3) {// Soma
+                    int numero5 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
+                    int numero6 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
 
-          Integer.parseInt(JOptionPane.showInputDialog(null,"O resultado  "
-                                                                       +(numero5/numero6)));}
-                                                                       
-          else   if(acao1==4){//Soma                                                            
- int numero7= Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
-            int numero8 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
+                    Integer.parseInt(JOptionPane.showInputDialog(null, "O resultado  "
+                            + (numero5 / numero6)));
+                }
 
-          Integer.parseInt(JOptionPane.showInputDialog(null,"O resultado  "
-                                                                       +(numero7*numero8)));  }                                                                                                           
-                 else{
-                 rodando=false; 
-                 }                                                                                                                 
-               
-                 
-                 
-                } catch (NumberFormatException erro1) {
-                    JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro"
-                            + "\nDigite novamente");
-                } catch (ArithmeticException erro2) {
-                    JOptionPane.showMessageDialog(null, "Erro - não Dividirás por Zero!!!"
-                            + "\nDigite novamente");
-                } catch (Exception erro2) {
-                    JOptionPane.showMessageDialog(null, "Aconteceu um erro"
-                            + "\nDigite novamente");
-                        }
+                else if (acao1 == 4) {// Soma
+                    int numero7 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
+                    int numero8 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
+
+                    Integer.parseInt(JOptionPane.showInputDialog(null, "O resultado  "
+                            + (numero7 * numero8)));
+                } else {
+                    rodando = false;
+                }
+
+            } catch (NumberFormatException erro1) {
+                JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro");
+                      
+            } catch (ArithmeticException erro2) {
+                JOptionPane.showMessageDialog(null, "Erro - não Dividirás por Zero!!!");
+                       
+            } catch (Exception erro3) {
+                JOptionPane.showMessageDialog(null, "Aconteceu um erro");
+                       
+            }finally   {
+
+              JOptionPane.showMessageDialog(null, "Tente Novamente");
+                      
+
+    }
 
 }
-
-
 }
