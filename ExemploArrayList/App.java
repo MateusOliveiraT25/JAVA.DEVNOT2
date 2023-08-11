3e898a2d78d00c909baa0732575904e18127d8c0
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-08-10 21:53:21 -0300

    10/08

---
+++ ExemploArrayList/App.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * App
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        ExemploArrayList obj1 =new ExemploArrayList();
+        obj1.exercicio();
+        ExemploHachMap obj2 = new ExemploHachMap();
+        obj2.teste();
+      
+    }
+    
+}
---
+++ ExemploArrayList/ExemploLinkedList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+public class ExemploLinkedList {
+    LinkedList<String> pessoas = new LinkedList();
+ //métodos
+ public void teste(){
+}
+}
