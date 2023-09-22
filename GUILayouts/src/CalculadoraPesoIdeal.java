a3a887c16ec94979e1e5f33ba7903b9100f550bb
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-09-22 09:40:49 -0300

    22/09

--- GUILayouts/src/CalculadoraPesoIdeal.java
+++ GUILayouts/src/CalculadoraPesoIdeal.java
@@ -21,6 +21,7 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.anchor = GridBagConstraints.WEST;
 
+        // Rótulo e campo de entrada para altura ou nome
         gbc.gridx = 0;
         gbc.gridy = 0;
         add(new JLabel("Inserir Altura (metros) ou Nome:"), gbc);
@@ -29,6 +30,7 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
         entradaField = new JTextField(10);
         add(entradaField, gbc);
 
+        // Rótulo e caixa de seleção para sexo
         gbc.gridx = 0;
         gbc.gridy = 1;
         add(new JLabel("Escolher Sexo:"), gbc);
@@ -38,6 +40,7 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
         sexoComboBox = new JComboBox<>(sexoOptions);
         add(sexoComboBox, gbc);
 
+        // Botões numéricos e letras
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
@@ -45,8 +48,13 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
         criarBotoesNumericos();
         criarBotoesLetras();
         adicionarBotaoAlternar();
-        add(botoesNumericosPanel, gbc);
 
+        // Crie um painel principal para os botões com uma grade de 2x1
+        JPanel botoesPanel = new JPanel(new GridLayout(1, 2, 5, 5));
+        botoesPanel.add(entradaNumerica ? botoesNumericosPanel : botoesLetrasPanel);
+        add(botoesPanel, gbc);
+
+        // Botão Apagar
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.gridwidth = 1;
@@ -55,6 +63,7 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
         apagarButton.addActionListener(this);
         add(apagarButton, gbc);
 
+        // Botão Calcular
         gbc.gridx = 1;
         gbc.anchor = GridBagConstraints.EAST;
         calcularButton = new JButton("Calcular");
@@ -63,7 +72,7 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
     }
 
     private void criarBotoesNumericos() {
-        botoesNumericosPanel = new JPanel(new GridLayout(4, 7, 5, 5));
+        botoesNumericosPanel = new JPanel(new GridLayout(4, 4, 5, 5));
 
         for (int i = 1; i <= 9; i++) {
             JButton botao = new JButton(String.valueOf(i));
@@ -71,6 +80,7 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
             botoesNumericosPanel.add(botao);
         }
 
+        // Botões adicionais
         botoesNumericosPanel.add(new JButton("!"));
         botoesNumericosPanel.add(new JButton("@"));
         botoesNumericosPanel.add(new JButton("#"));
@@ -92,9 +102,9 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
     }
 
     private void criarBotoesLetras() {
-        botoesLetrasPanel = new JPanel(new GridLayout(3, 4, 5, 5));
+        botoesLetrasPanel = new JPanel(new GridLayout(4, 4, 5, 5));
 
-        for (char letra = 'A'; letra <= 'Z'; letra++) {
+        for (char letra = 'A'; letra <= 'P'; letra++) {
             JButton botao = new JButton(String.valueOf(letra));
             botao.addActionListener(this);
             botoesLetrasPanel.add(botao);
@@ -102,14 +112,25 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
     }
 
     private void adicionarBotaoAlternar() {
-        alternarButton = new JButton("Alternar para Letras");
+        alternarButton = new JButton(entradaNumerica ? "Alternar para Letras" : "Alternar para Números");
         alternarButton.addActionListener(this);
-        botoesNumericosPanel.add(alternarButton);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Implementação do actionPerformed (não incluído no código completo para economizar espaço)
+        if (e.getSource() == alternarButton) {
+            entradaNumerica = !entradaNumerica;
+            alternarButton.setText(entradaNumerica ? "Alternar para Letras" : "Alternar para Números");
+
+            // Atualiza o conjunto correto de botões com base na escolha do usuário
+            JPanel botoesPanel = (JPanel) getComponent(2);
+            botoesPanel.removeAll();
+            botoesPanel.add(entradaNumerica ? botoesNumericosPanel : botoesLetrasPanel);
+            botoesPanel.revalidate();
+            botoesPanel.repaint();
+        } else {
+            // Implementação do actionPerformed (lógica para os outros botões)
+        }
     }
 
     public static void main(String[] args) {
@@ -121,4 +142,3 @@ public class CalculadoraPesoIdeal extends JPanel implements ActionListener {
         frame.setVisible(true);
     }
 }
-
