2a352d05aca808b060f3528a77a6120cd150a4a0
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-09-29 15:59:44 -0300

    29/09

--- GUICardTabed/src/ExercicioCardLayout4.java
+++ GUICardTabed/src/ExercicioCardLayout4.java
@@ -1,42 +1,55 @@
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class BarChartExample extends JFrame {
+public class ChartWithTabs extends JFrame {
 
-    public BarChartExample(String title) {
+    public ChartWithTabs(String title) {
         super(title);
 
-        // Criação do conjunto de dados
-        CategoryDataset dataset = createDataset();
+        // Criação do JTabbedPane
+        JTabbedPane tabbedPane = new JTabbedPane();
 
-        // Criação do gráfico
-        JFreeChart chart = ChartFactory.createBarChart(
-                "Exemplo de Gráfico de Barras", // Título do gráfico
-                "Categorias",                   // Rótulo do eixo X
-                "Valores",                      // Rótulo do eixo Y
-                dataset                         // Conjunto de dados
+        // Criação do gráfico de barras e sua aba
+        CategoryDataset barDataset = createBarDataset();
+        JFreeChart barChart = ChartFactory.createBarChart(
+                "Gráfico de Barras",
+                "Categorias",
+                "Valores",
+                barDataset
         );
+        ChartPanel barChartPanel = new ChartPanel(barChart);
+        tabbedPane.addTab("Gráfico de Barras", barChartPanel);
 
-        // Configuração opcional do gráfico (por exemplo, adicionar cores, legendas, etc.)
+        // Criação do gráfico de pizza e sua aba
+        DefaultPieDataset pieDataset = createPieDataset();
+        JFreeChart pieChart = ChartFactory.createPieChart(
+                "Gráfico de Pizza",
+                pieDataset,
+                true, // Lendas
+                true,
+                false
+        );
+        ChartPanel pieChartPanel = new ChartPanel(pieChart);
+        tabbedPane.addTab("Gráfico de Pizza", pieChartPanel);
 
-        // Criação do painel de gráfico
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(560, 370));
+        // Adição do JTabbedPane ao JFrame
+        add(tabbedPane);
 
-        // Adição do painel de gráfico ao JFrame
-        setContentPane(chartPanel);
+        // Configurações da janela
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private CategoryDataset createDataset() {
+    private CategoryDataset createBarDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-        // Adição de dados ao conjunto de dados
         dataset.addValue(1.0, "Série1", "Categoria1");
         dataset.addValue(4.0, "Série1", "Categoria2");
         dataset.addValue(3.0, "Série1", "Categoria3");
@@ -45,14 +58,23 @@ public class BarChartExample extends JFrame {
         return dataset;
     }
 
+    private DefaultPieDataset createPieDataset() {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+
+        dataset.setValue("Categoria1", 1.0);
+        dataset.setValue("Categoria2", 4.0);
+        dataset.setValue("Categoria3", 3.0);
+        dataset.setValue("Categoria4", 5.0);
+
+        return dataset;
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            BarChartExample example = new BarChartExample("Exemplo de Gráfico de Barras");
-            example.setSize(800, 600);
-            example.setLocationRelativeTo(null);
-            example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            ChartWithTabs example = new ChartWithTabs("Gráficos com Abas");
             example.setVisible(true);
         });
     }
 }
+
 
