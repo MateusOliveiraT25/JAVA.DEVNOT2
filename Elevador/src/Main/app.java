c184e0a4f396eb6c164d3295ac0ec01b7abe7557
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-02-02 21:18:59 -0300

    02/02/24

--- Elevador/src/Main/app.java
+++ Elevador/src/Main/app.java
@@ -1,10 +1,9 @@
 package Main;
 
 import javax.swing.SwingUtilities;
-
 import View.PainelElevador;
 
-public class app {
+public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new PainelElevador().setVisible(true));
     }
